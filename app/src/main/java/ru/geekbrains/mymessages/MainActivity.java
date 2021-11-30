bdafddcf85c3448fe4ce48c5863ee11df42a4e25
DwarfPorter <mrvlmor@gmail.com>
2021-11-30 20:39:15 +0300

    Show snackbar with actiob

--- app/src/main/java/ru/geekbrains/mymessages/MainActivity.java
+++ app/src/main/java/ru/geekbrains/mymessages/MainActivity.java
@@ -20,10 +20,19 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button_snakbar).setOnClickListener(view -> {
             showSnackbar();
         });
+        findViewById(R.id.button_snakbar_with_action).setOnClickListener(view -> {
+            showSnackbarWithAction();
+        });
     }
 
     private void showSnackbar() {
-        Snackbar.make(findViewById(R.id.container), "Show messge in snack bar", Snackbar.LENGTH_LONG).show();
+        Snackbar.make(findViewById(R.id.container), "Show message in snack bar", Snackbar.LENGTH_LONG).show();
+    }
+
+    private void showSnackbarWithAction() {
+        Snackbar.make(findViewById(R.id.container), "Show message with action", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Push me", view -> { showToast();})
+                .show();
     }
 
     private void showToast() {
